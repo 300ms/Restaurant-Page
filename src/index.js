eb67a3e8d9b1b00267306a1338b7e80a44622414
300ms <ilhansnmz44@gmail.com>
2020-11-23 17:53:11 +0300

    Make everything included navbar and nav/tab buttons dynamically created by js as requested

--- src/index.js
+++ src/index.js
@@ -12,8 +12,37 @@ document.getElementById('content').style.backgroundRepeat = 'no-repeat';
 document.getElementById('content').style.backgroundPosition = 'center top';
 document.getElementById('content').style.backgroundSize = '100% 100%';
 
+const createNav = () => {
+  const nav = document.createElement('nav');
+  nav.className += 'navbar navbar-expand-lg navbar-dark bg-primary';
+  nav.id = 'nav';
+  nav.innerHTML = `
+  <a class="navbar-brand" href="#">Restaurant</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarColor01">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" id="homePage" href="#">Home
+          <span class="sr-only">(current)</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" id="menuPage" href="#">Menu</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" id="contactPage" href="#">Contact</a>
+      </li>
+    </ul>
+  </div>`;
+  document.body.insertBefore(nav, document.getElementById('content'));
+};
+
 const clearContainer = () => { document.getElementById('content').innerHTML = ''; };
 
+createNav();
 Home.homePage();
 
 document.getElementById('homePage').addEventListener('click', () => {
